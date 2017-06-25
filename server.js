a8d7a6f7d53a5ad92d1dcd325c321212a2512b2d
Anoop Singh <anoop100singh@gmail.com>
2017-06-25 08:01:24 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,22 @@
+var express=require('express');
+var app = express()
+var http = require('http').Server(app)
+var io = require('socket.io') (http)
+
+app.use(express.static(__dirname + '/Client'));
+
+io.on('connection', function(socket){
+	console.log('user connected')
+	socket.on('chat message', function(message){
+		io.emit('chat message', message)
+	})
+	
+	socket.on('disconnect', function(){
+		console.log('user disconnected')
+	})
+	
+})
+
+http.listen(3000, function(){
+	console.log('listening on *:3000') 
+})
